92c3ae6afdde87b4a5efce5a2b4523796a7c52fe
Huey Nguyen <syndrumablez@gmail.com>
2023-08-19 02:47:43 -0700

    Signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,11 @@
 // backend/routes/api/users.js
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const { check } = require('express-validator');
+const { check } = require("express-validator");
 const { Op } = require("sequelize");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { handleValidationErrors } = require('../../utils/validation');
+const { handleValidationErrors } = require("../../utils/validation");
 const { User } = require("../../db/models");
 
 const router = express.Router();
@@ -65,7 +65,7 @@ router.post("/login", validateLogin, async (req, res, next) => {
     username: user.username,
     firstName: user.firstName,
     lastName: user.lastName,
-    imgUrl: user.imgUrl
+    imgUrl: user.imgUrl,
   };
 
   await setTokenCookie(res, safeUser);
@@ -75,16 +75,22 @@ router.post("/login", validateLogin, async (req, res, next) => {
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-  const { email, password, username, firstName, lastName } = req.body;
+  const { email, username, firstName, lastName, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, firstName, lastName, username, hashedPassword });
+  const user = await User.create({
+    email,
+    username,
+    firstName,
+    lastName,
+    hashedPassword,
+  });
 
   const safeUser = {
     id: user.id,
     email: user.email,
+    username: user.username,
     firstName: user.firstName,
     lastName: user.lastName,
-    username: user.username,
   };
 
   await setTokenCookie(res, safeUser);
