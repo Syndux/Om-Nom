2ca762587f935e85ac8df7d53c09b4e62f37c3e5
Huey Nguyen <syndrumablez@gmail.com>
2023-08-19 03:09:37 -0700

    Moved validations to /utils/validation.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,42 +1,13 @@
 // backend/routes/api/users.js
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const { check } = require("express-validator");
 const { Op } = require("sequelize");
 
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie } = require("../../utils/auth");
+const { validateLogin, validateSignup }  = require("../../utils/validation");
 const { User } = require("../../db/models");
 
 const router = express.Router();
-
-const validateLogin = [
-  check("credential")
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage("Please provide a valid email or username."),
-  check("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a password."),
-  handleValidationErrors,
-];
-
-const validateSignup = [
-  check("email")
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage("Please provide a valid email."),
-  check("username")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("password")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage("Password must be 6 characters or more."),
-  handleValidationErrors,
-];
 
 // Restore session user
 router.get("/current", (req, res) => {
