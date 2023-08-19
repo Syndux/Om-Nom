944b9c5bdca00d887c7c42b789356979ea128131
Huey Nguyen <syndrumablez@gmail.com>
2023-08-18 21:52:56 -0700

    LoginRoute

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,5 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
-const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -8,8 +7,6 @@ const { restoreUser } = require("../../utils/auth.js");
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
-router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,12 +2,24 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const { check } = require('express-validator');
+const { Op } = require("sequelize");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require("../../db/models");
 
 const router = express.Router();
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
 
 const validateSignup = [
   check("email")
@@ -25,6 +37,41 @@ const validateSignup = [
     .withMessage("Password must be 6 characters or more."),
   handleValidationErrors,
 ];
+
+// Login
+router.post("/login", validateLogin, async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    imgUrl: user.imgUrl
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({ user: safeUser });
+});
 
 // Sign up
 router.post("", validateSignup, async (req, res) => {
