0903b61a87c40d3bc5615537b0ed4011ccd82c24
Huey Nguyen <syndrumablez@gmail.com>
2023-08-22 20:06:23 -0700

    Ingredients seeder

--- backend/db/seeders/20230821053145-ingredients.js
+++ backend/db/seeders/20230821053145-ingredients.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 
 let options = {};
@@ -11,11 +11,77 @@ module.exports = {
     options.tableName = "Ingredients";
     return queryInterface.bulkInsert(
       options,
-      []
-    )
+      [
+        {
+          name: "Soy Sauce",
+          imgUrl: "",
+        },
+        {
+          name: "White Sugar",
+          imgUrl: "",
+        },
+        {
+          name: "Brown Sugar",
+          imgUrl: "",
+        },
+        {
+          name: "Garlic",
+          imgUrl: "",
+        },
+        {
+          name: "Green Onion",
+          imgUrl: "",
+        },
+        {
+          name: "Sesame Seed",
+          imgUrl: "",
+        },
+        {
+          name: "Rib Eye Steak",
+          imgUrl: "",
+        },
+        {
+          name: "Salt",
+          imgUrl: "",
+        },
+        {
+          name: "Black Pepper",
+          imgUrl: "",
+        },
+        {
+          name: "Glutinous White Rice",
+          imgUrl: "",
+        },
+        {
+          name: "Water",
+          imgUrl: "",
+        },
+        {
+          name: "Spinach",
+          imgUrl: "",
+        },
+        {
+          name: "Cucumber",
+          imgUrl: "",
+        },
+        {
+          name: "Sesame Oil",
+          imgUrl: "",
+        },
+        {
+          name: "Egg",
+          imgUrl: "",
+        },
+        {
+          name: "Onion",
+          imgUrl: "",
+        },
+      ],
+      { validate: true }
+    );
   },
 
-  down: async  (queryInterface, Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
     options.tableName = "Ingredients";
     return queryInterface.bulkDelete(options, null, {});
   },
