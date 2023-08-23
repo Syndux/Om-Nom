2a0e9bc04909d98a50c87c7a3045acd1c814886b
Huey Nguyen <syndrumablez@gmail.com>
2023-08-23 00:35:40 -0700

    Add mealsRouter to express app

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
 const usersRouter = require("./users.js");
+const mealsRouter = require("./meals.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -9,6 +10,7 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use("/users", usersRouter);
+router.use("/meals", mealsRouter);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
