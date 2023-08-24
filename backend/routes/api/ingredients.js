8201db3805fe0df6aeac508aaa221c155dd9e8d7
Huey Nguyen <syndrumablez@gmail.com>
2023-08-23 22:32:36 -0700

    Added getAllIngredients route

--- backend/routes/api/ingredients.js
+++ backend/routes/api/ingredients.js
@@ -5,4 +5,12 @@ const { Ingredients } = require("../../db/models");
 
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  const ingredients = await Ingredients.findAll({
+    order: ["name"],
+  });
+
+  return res.json(ingredients);
+});
+
 module.exports = router;
