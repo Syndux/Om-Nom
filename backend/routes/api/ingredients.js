08ce22e39d06ce7ad99325896831d0fe99f40dc2
Huey Nguyen <syndrumablez@gmail.com>
2023-08-23 22:34:04 -0700

    Fixed error referencing Ingredients model

--- backend/routes/api/ingredients.js
+++ backend/routes/api/ingredients.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 const { requireAuth } = require("../../utils/auth");
-const { Ingredients } = require("../../db/models");
+const { Ingredient } = require("../../db/models");
 
 const router = express.Router();
 
 // Get all ingredients
 router.get("/", async (req, res, next) => {
-  const ingredients = await Ingredients.findAll({
+  const ingredients = await Ingredient.findAll({
     order: ["name"],
   });
 
