9aae16bd93faaddce54ca3e74c1889bbf95ceb55
Huey Nguyen <syndrumablez@gmail.com>
2023-08-31 21:28:35 -0700

    Sidebar component rafce

---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Link to="/">
+        Om Nom
+      </Link>
+    </div>
+  )
+};
+
+export default Sidebar;
