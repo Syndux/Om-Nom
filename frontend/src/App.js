b138b8becfa560b1df2b93b341f412a713a531d5
Huey Nguyen <syndrumablez@gmail.com>
2023-09-01 00:29:49 -0700

    New sitemap structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,12 +35,38 @@ function App() {
     // </>
     <div>
       {isLoaded && activeMenu ? (
-        <div className="sidebar fixed w-60 bg-white">Active Sidebar</div>
-      ) : (
-        <div className="w-0">
-          Inactive Sidebar
+        <div className="sidebar fixed w-60 bg-white dark:bg-secondary-dark-bg">
+          Active Sidebar
         </div>
+      ) : (
+        <div className="w-0 dark:bg-secondary-dark-bg">Inactive Sidebar</div>
       )}
+      <div
+        className={`min-h-screen w-full bg-main-bg dark:bg-main-bg ${
+          activeMenu ? `md:ml-72` : `flex-2`
+        }`}
+      >
+        <div className="navbar fixed w-full bg-main-bg dark:bg-main-dark-bg md:static">
+          NavBar
+        </div>
+      </div>
+
+      <div>
+        {/* Preparing to update ReactRouterDom v6 */}
+        <Switch>
+          {/* Home */}
+          <Route exact path="/" component={LandingPage} /> {/* View for logged-in (explore) and logged-out (showcase) */}
+          {/* Meals */}
+          <Route path="/meals/current" component={} /> {/* SIDEBAR - View all meals */}
+          <Route path="/meals/saved" component={} /> {/* SIDEBAR - View favorite/saved meals - Coming soon!*/}
+          <Route path="/meals/:mealId" component={} /> {/* View singular meal */}
+          <Route path="/meals" component={} /> {/* SIDEBAR - View all meals */}
+          {/* Ingredients */}
+          <Route path="/ingredients" component={} /> {/* SIDEBAR - View all ingredients */}
+          {/* Meal Planning */}
+          <Route path="/meal-plan" component={} /> {/* SIDEBAR - Calendar for meals - Coming soon!*/}
+        </Switch>
+      </div>
     </div>
   );
 }
