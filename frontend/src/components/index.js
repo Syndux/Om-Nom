0d66a467d0edb794f291d3db5f486c2c32b487e3
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 16:11:14 -0700

    Export components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -6,4 +6,17 @@ export { default as Ingredients } from './Ingredients';
 export { default as LandingPage } from './LandingPage';
 export { default as Sidebar } from './Sidebar';
 
-// Did not export components in folders
+// Folder exports
+export { default as LoginFormPage } from './Login-Signup/LoginFormPage';
+export { default as SignupFormPage } from './Login-Signup/SignupFormPage';
+export { default as Meals } from './Meals';;
+export { default as Meal } from './Meals/Meal';
+export { default as MealsOwned}  from './Meals/MealsOwned';
+export { default as Navbar } from './Navbar';
+export { default as GroceryList } from './Navbar/GroceryList';
+export { default as Notification } from './Navbar/Notification';
+export { default as ThemeToggle } from './Navbar/ThemeToggle';
+export { default as UserProfile } from './Navbar/UserProfile';
+
+// export { default as OpenModalMenuItem } from './Navbar/OpenModalMenuItem';
+// export { default as ProfileButton } from './Navbar/ProfileButton';
