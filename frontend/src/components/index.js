b80a825c8e399fe29f97b30428d54e5119d609c8
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 15:47:26 -0700

    initialize index.js for components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Button } from './Button';
+export { default as ComingSoon } from './ComingSoon';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Ingredients } from './Ingredients';
+export { default as LandingPage } from './LandingPage';
+export { default as Sidebar } from './Sidebar';
+
+// Did not export components in folders
