5d5df2d888a172a47d18c60b0dbbdfe4dd640bd5
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 16:10:56 -0700

    Restructure app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import ComingSoon from "./components/ComingSoon";
-import LandingPage from "./components/LandingPage";
-import LoginFormPage from "./components/Login-Signup/LoginFormPage";
-import SignupFormPage from "./components/Login-Signup/SignupFormPage";
-import Meals from "./components/Meals";
-import Meal from "./components/Meals/Meal";
-import MealsOwned from "./components/Meals/MealsOwned";
-import Navbar from "./components/Navbar";
-import Ingredients from "./components/Ingredients";
-import Sidebar from "./components/Sidebar";
+import {
+  ComingSoon,
+  LandingPage,
+  Meals,
+  Meal,
+  MealsOwned,
+  Navbar,
+  Ingredients,
+  Sidebar,
+  ThemeToggle,
+} from "./components";
 
 import * as sessionActions from "./store/session";
 
@@ -26,46 +27,63 @@ function App() {
   const activeMenu = true;
 
   return (
-    <div>
-      {isLoaded && activeMenu ? (
-        <div className="sidebar fixed w-60 bg-white dark:bg-secondary-dark-bg">
-          <Sidebar />
+    <div className={currentMode === "Dark" ? "dark" : ""}>
+      <div className="relative flex dark:bg-main-dark-bg">
+        {isLoaded && activeMenu ? (
+          <div className="sidebar fixed w-60 bg-white dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        )}
+        <div
+          className={`min-h-screen w-full bg-main-bg dark:bg-main-bg ${
+            activeMenu ? `md:ml-60` : `flex-2`
+          }`}
+        >
+          <div className="navbar fixed w-full bg-main-bg dark:bg-main-dark-bg md:static">
+            <Navbar />
+          </div>
         </div>
-      ) : (
-        <div className="w-0 dark:bg-secondary-dark-bg">
-          <Sidebar />
-        </div>
-      )}
-      <div
-        className={`min-h-screen w-full bg-main-bg dark:bg-main-bg ${
-          activeMenu ? `md:ml-60` : `flex-2`
-        }`}
-      >
-        <div className="navbar fixed w-full bg-main-bg dark:bg-main-dark-bg md:static">
-          <Navbar />
-        </div>
-      </div>
         {/* Preparing to update ReactRouterDom v6 */}
         <Switch>
           {/* Home */}
-          <Route exact path="/" component={LandingPage} /> {/* View for logged-in (explore) and logged-out (showcase) */}
+          {/* View for logged-in (explore) and logged-out (showcase) */}
+          <Route exact path="/" component={LandingPage} />{" "}
+          
           {/* Meals */}
-          <Route path="/meals/current" component={MealsOwned} /> {/* SIDEBAR - View all of your meals */}
-          <Route path="/meals/saved" component={ComingSoon} /> {/* SIDEBAR - View favorite/saved meals - Coming soon! */}
-          <Route path="/meals/:mealId" component={Meal} /> {/* View singular meal */}
-          <Route path="/meals" component={Meals} /> {/* SIDEBAR - View all meals */}
+          {/* SIDEBAR - View all of your meals */}
+          <Route path="/meals/current" component={MealsOwned} />
+          {/* SIDEBAR - View favorite/saved meals - Coming soon! */}
+          <Route path="/meals/saved" component={ComingSoon} />
+          {/* View singular meal */}
+          <Route path="/meals/:mealId" component={Meal} />
+          {/* SIDEBAR - View all meals */}
+          <Route path="/meals" component={Meals} />
+          
           {/* Ingredients */}
-          <Route path="/ingredients" component={Ingredients} /> {/* SIDEBAR - View all ingredients */}
+          {/* SIDEBAR - View all ingredients */}
+          <Route path="/ingredients" component={Ingredients} />
+          
           {/* Meal Planning */}
-          <Route path="/meal-plan" component={ComingSoon} /> {/* SIDEBAR - Calendar for meals - Coming soon!*/}
+          {/* SIDEBAR - Calendar for meals - Coming soon!*/}
+          <Route path="/meal-plan" component={ComingSoon} />
+          
           {/* User Profile */}
-          <Route path="/following" component={ComingSoon} /> {/* NAVBAR - View followers/following after clicking text in profile dropdown - Coming soon! */}
-          <Route path="/reviews" component={ComingSoon} /> {/* NAVBAR - View all reviews you made - Coming soon! */}
-          <Route path="/messages" component={ComingSoon} /> {/* NAVBAR - View your messages - Coming soon! */}
+          {/* NAVBAR - View followers/following after clicking text in profile dropdown - Coming soon! */}
+          <Route path="/following" component={ComingSoon} />
+          {/* NAVBAR - View all reviews you made - Coming soon! */}
+          <Route path="/reviews" component={ComingSoon} />
+          {/* NAVBAR - View your messages - Coming soon! */}
+          <Route path="/messages" component={ComingSoon} />
+          {/* Logout */}
           <Route path="/logout">
-            <Redirect to="/"/>
+            <Redirect to="/" />
           </Route>
         </Switch>
+      </div>
     </div>
   );
 }
