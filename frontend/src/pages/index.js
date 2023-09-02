32b8e69f478811109d75dd6068754cdce76bc2c4
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 16:58:32 -0700

    App restructuring

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,1 +1,8 @@
-export { default as LandingPage } from './LandingPage';
+export { default as Showcase } from './Home_Landing/Showcase';
+export { default as Explore } from './Home_Landing/Explore';
+
+export { default as AllMealsPage } from './Meals/AllMealsPage';
+export { default as FavoriteMealsPage } from './Meals/FavoriteMealsPage';
+export { default as OwnedMealsPage } from './Meals/OwnedMealsPage';
+
+export { default as AllIngredientsPage } from './Ingredients/AllIngredientsPage';
