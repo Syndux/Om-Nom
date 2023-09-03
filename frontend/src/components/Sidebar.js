e32e4f456bee2ebb80ea26af6c7dcb5a0e46ec6c
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 20:32:31 -0700

    Added sidebar items with styling

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -14,6 +14,7 @@ const sidebarItems = [
   {
     text: "Home",
     icon: <FaHouse />,
+    link: "home",
   },
   {
     text: "Meals",
@@ -22,14 +23,17 @@ const sidebarItems = [
   {
     text: "All Meals",
     icon: <BiSolidFoodMenu />,
+    link: "meals",
   },
   {
     text: "Your Meals",
     icon: <PiBowlFood />,
+    link: "meals/current",
   },
   {
     text: "Your Saved Meals",
     icon: <FaBookmark />,
+    link: "meals/saved",
   },
   {
     text: "Ingredients",
@@ -38,6 +42,7 @@ const sidebarItems = [
   {
     text: "All Ingredients",
     icon: <FaCarrot />,
+    link: "ingredients",
   },
   {
     text: "Meal Planning",
@@ -46,33 +51,59 @@ const sidebarItems = [
   {
     text: "Meal Schedule",
     icon: <FaCalendarDays />,
+    link: "meal-plan",
   },
 ];
 
 // Temp vars
 const sidebarOpen = true;
+const activePage =
+  "flex items-center gap-4 pl-3 pt-2 pb-2 rounded-lg text-white text-md m-2";
+const inactivePage =
+  "flex items-center gap-4 pl-3 pt-2 pb-2 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
 const Sidebar = () => {
   return (
-    <div className="ml-3 h-screen overflow-auto pb-10 md:overflow-hidden md:hover:overflow-auto">
+    <div className="mx-2 h-screen overflow-auto pb-10 md:overflow-hidden md:hover:overflow-auto">
       {sidebarOpen && (
         <>
-          <div className="flex items-center justify-between">
-            <Link to="/">
-              <img src={OmNomDarkLogo} alt="Om Nom logo" />
+          <div className="flex items-center justify-center">
+            <Link to="/" onClick={() => {}} className="mt-4">
+              <img
+                src={OmNomDarkLogo}
+                alt="Om Nom logo"
+                className="h-20 w-20"
+              />
             </Link>
           </div>
-          <div className="mt-10">
-            {sidebarItems.map(({ text, icon }) => {
+          <div className="mt-8">
+            {sidebarItems.map(({ text, icon, link }) => {
               if (!icon) {
                 return (
                   <div key={text}>
-                    <p className="m-3 mt-4 uppercase text-gray-400 dark:text-gray-400">
+                    <p className="m-3 ml-2 mt-4 uppercase text-gray-600 dark:text-gray-600">
                       {text}
                     </p>
                   </div>
                 );
               }
+
+              return (
+                <NavLink
+                  to={`/${link}`}
+                  key={text}
+                  onClick={() => {}}
+                  // style={({ isActive }) => ({
+                  //   backgroundColor: isActive ? currentColor : "",
+                  // })}
+                  className={({ isActive }) =>
+                    isActive ? activePage : inactivePage
+                  }
+                >
+                  {icon}
+                  <span>{text}</span>
+                </NavLink>
+              );
             })}
           </div>
         </>
