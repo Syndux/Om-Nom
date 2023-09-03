29a517aa27c43550cee231701959ec061dd7ae82
Huey Nguyen <syndrumablez@gmail.com>
2023-09-02 18:11:33 -0700

    Moved Login-Signup

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,15 +1,9 @@
 export { default as Button } from './Button';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as Ingredients } from './Ingredients';
 export { default as Sidebar } from './Sidebar';
 
 // Folder exports
-export { default as LoginFormPage } from './Login-Signup/LoginFormPage';
-export { default as SignupFormPage } from './Login-Signup/SignupFormPage';
-export { default as Meals } from './Meals';;
-export { default as Meal } from './Meals/Meal';
-export { default as MealsOwned}  from './Meals/MealsOwned';
 export { default as Navbar } from './Navbar';
 export { default as GroceryList } from './Navbar/GroceryList';
 export { default as Notification } from './Navbar/Notification';
