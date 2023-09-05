51c4171c1b1362f26697c3e43df6b759f7c404bd
Huey Nguyen <syndrumablez@gmail.com>
2023-09-04 23:50:29 -0700

    style adjust

--- frontend/src/pages/Meals/AllMealsPage.js
+++ frontend/src/pages/Meals/AllMealsPage.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const AllMealsPage = () => {
   return (
-    <div className="mt-10">
-      <div className="flex flex-wrap justify-center lg:flex-nowrap ">
-        <div className="dark:text-light-gray text-main-dark-bg m-3 h-screen w-full rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
+    <div className="my-2">
+      <div className="flex flex-wrap justify-center lg:flex-nowrap">
+        <div className="flex flex-col justify-center items-center dark:text-light-gray text-main-dark-bg m-3 h-screen w-full rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
           <p className="font-bold m-4 text-xl">Browse all available meals</p>
         </div>
       </div>
