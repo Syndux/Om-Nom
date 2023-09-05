a11356ea3f0e21060589a9d843c96c802aabfcba
Huey Nguyen <syndrumablez@gmail.com>
2023-09-04 23:52:19 -0700

    Style adjustments

--- frontend/src/pages/Home_Landing/ShowcasePage.js
+++ frontend/src/pages/Home_Landing/ShowcasePage.js
@@ -4,15 +4,19 @@ import { useHistory } from "react-router-dom";
 
 const Showcase = () => {
   const history = useHistory();
-  const sessionUser = useSelector(state => state.session.user) // User logged in?
+  const sessionUser = useSelector((state) => state.session.user); // User logged in?
 
   useEffect(() => {
     if (!sessionUser) history.push("/");
   }, [sessionUser, history]);
 
   return (
-    <div className="flex justify-center items-center h-full">
-      Showcase / Landing Page
+    <div className="my-2">
+      <div className="flex flex-wrap justify-center lg:flex-nowrap">
+        <div className="dark:text-light-gray text-main-dark-bg m-3 flex h-screen w-full flex-col items-center justify-center rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
+          <p className="m-4 text-xl font-bold">Showcase for all users</p>
+        </div>
+      </div>
     </div>
   );
 };
