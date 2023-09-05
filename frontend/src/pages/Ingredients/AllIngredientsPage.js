2b8c3b564b89d8d3da2dda0d6854c69fe99f62dc
Huey Nguyen <syndrumablez@gmail.com>
2023-09-04 23:50:05 -0700

    All ingredients starter

--- frontend/src/pages/Ingredients/AllIngredientsPage.js
+++ frontend/src/pages/Ingredients/AllIngredientsPage.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const AllIngredientsPage = () => {
   return (
-    <div className="mt-10">
-    <div className="flex flex-wrap justify-center lg:flex-nowrap ">
-      <div className="dark:text-light-gray text-main-dark-bg m-3 h-screen w-full rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
-        <p className="font-bold m-4 text-xl">View all available ingredients</p>
+    <div className="my-2">
+      <div className="flex flex-wrap justify-center lg:flex-nowrap">
+        <div className="dark:text-light-gray text-main-dark-bg m-3 flex h-screen w-full flex-col items-center justify-center rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
+          <p className="m-4 text-xl font-bold">
+            View all available ingredients
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default AllIngredientsPage;
