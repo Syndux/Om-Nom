7580555b3814be0389965861db703e17d943e3db
Huey Nguyen <syndrumablez@gmail.com>
2023-09-04 23:49:06 -0700

    All avail ingredients starter

--- frontend/src/pages/Ingredients/AllIngredientsPage.js
+++ frontend/src/pages/Ingredients/AllIngredientsPage.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const AllIngredientsPage = () => {
   return (
-    <div>AllIngredientsPage</div>
+    <div className="mt-10">
+    <div className="flex flex-wrap justify-center lg:flex-nowrap ">
+      <div className="dark:text-light-gray text-main-dark-bg m-3 h-screen w-full rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
+        <p className="font-bold m-4 text-xl">View all available ingredients</p>
+      </div>
+    </div>
+  </div>
   )
 }
 
