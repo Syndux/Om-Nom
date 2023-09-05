404ef7476e42af690cd536914cf481c8749cd5c2
Huey Nguyen <syndrumablez@gmail.com>
2023-09-05 00:22:19 -0700

    meals -> foods in store

--- frontend/src/store/foods.js
+++ frontend/src/store/foods.js
@@ -1,6 +1,6 @@
 import { csrfFetch } from './csrf';
 
-const GET_FOODS = 'meals/GET_FOODS';
+const GET_ALL_FOODS = 'foods/GET_ALL_FOODS';
 
 
 const initialState = {};
@@ -8,7 +8,7 @@ const initialState = {};
 const foodsReducer = (state = initialState, action) => {
   let newState = { ...state };
   switch (action.type) {
-    case SOME_CASE:
+    case GET_ALL_FOODS:
       return newState;
     default:
       return state;
