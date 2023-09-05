c38010144ecdd6cc72641a40a1b7f2cedb05d327
Huey Nguyen <syndrumablez@gmail.com>
2023-09-05 03:37:02 -0700

    sequelize plural or not

--- frontend/src/pages/Foods/AllFoodsPage.js
+++ frontend/src/pages/Foods/AllFoodsPage.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import getAllFoods from "../../store/foods";
 
 const AllFoodsPage = () => {
+  const dispatch = useDispatch();
+  const foods = useSelector((state) => state.foods);
+
+  useEffect(() => {
+    dispatch(getAllFoods());
+  }, [dispatch]);
+
   return (
     <div className="dark:text-light-gray text-secondary-dark-bg bg-light-gray dark:bg-secondary-dark-bg">
       <div className="flex flex-wrap justify-center lg:flex-nowrap">
         <div className="m-3 flex h-screen w-full flex-col items-center justify-center rounded-xl bg-main-bg dark:bg-main-dark-bg">
           <p className="m-4 text-xl font-bold">Browse all available foods</p>
+          {/* <p>{foods}</p> */}
         </div>
       </div>
     </div>
