0422f3300d00a1d6410c00356a76a22e6b59fc37
Huey Nguyen <syndrumablez@gmail.com>
2023-09-04 23:47:20 -0700

    Owned meals starter

--- frontend/src/pages/Meals/OwnedMealsPage.js
+++ frontend/src/pages/Meals/OwnedMealsPage.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const OwnedMealsPage = () => {
   return (
-    <div>OwnedMealsPage</div>
+    <div className="mt-10">
+    <div className="flex flex-wrap justify-center lg:flex-nowrap ">
+      <div className="dark:text-light-gray text-main-dark-bg m-3 h-screen w-full rounded-xl bg-light-gray dark:bg-secondary-dark-bg">
+        <p className="font-bold m-4 text-xl">Owned meals page</p>
+      </div>
+    </div>
+  </div>
   )
 }
 
