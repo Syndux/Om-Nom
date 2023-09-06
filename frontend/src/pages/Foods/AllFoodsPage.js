ff27f54fe034835287457ed749718503bf7bdad8
Huey Nguyen <syndrumablez@gmail.com>
2023-09-05 21:25:40 -0700

    All and owned foods

--- frontend/src/pages/Foods/AllFoodsPage.js
+++ frontend/src/pages/Foods/AllFoodsPage.js
@@ -18,14 +18,17 @@ const AllFoodsPage = () => {
   return (
     <div className="dark:text-light-gray text-secondary-dark-bg bg-light-gray dark:bg-secondary-dark-bg">
       <div className="flex flex-wrap justify-center lg:flex-nowrap">
-        <div className="m-3 flex h-screen w-full flex-col items-center rounded-xl bg-main-bg dark:bg-main-dark-bg">
+        <div className="m-3 flex w-full flex-col rounded-xl bg-main-bg dark:bg-main-dark-bg overflow-hidden overflow-y-scroll">
           {loaded && (
             <>
               <p className="m-4 text-xl font-bold">
                 Browse all available foods
               </p>
               {foods.map((food) => (
+                <div className="px-10 border-t">
                 <p>{food.name}</p>
+                <p>{food.cuisine}</p>
+              </div>
               ))}
             </>
           )}
--- frontend/src/pages/Foods/OwnedFoodsPage.js
+++ frontend/src/pages/Foods/OwnedFoodsPage.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { loadAllFoods } from "../../store/foods";
 
 const OwnedFoodsPage = () => {
+  const dispatch = useDispatch();
+  const foods = useSelector((state) => state.foods);
+  const [loaded, setLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(loadAllFoods());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
   return (
     <div className="dark:text-light-gray text-secondary-dark-bg bg-light-gray dark:bg-secondary-dark-bg">
       <div className="flex flex-wrap justify-center lg:flex-nowrap">
-        <div className="m-3 flex h-screen w-full flex-col items-center justify-center rounded-xl bg-main-bg dark:bg-main-dark-bg">
-          <p className="m-4 text-xl font-bold">Owned foods page</p>
+        <div className="m-3 flex h-screen w-full flex-col rounded-xl bg-main-bg dark:bg-main-dark-bg">
+          {sessionUser ? (
+            loaded && (
+              <>
+                <p className="m-4 text-xl font-bold">All owned foods</p>
+                {foods
+                  .filter((food) => food.creatorId === sessionUser.id)
+                  .map((food) => (
+                    <div className="px-10 border-b">
+                      <p>{food.name}</p>
+                      <p>{food.cuisine}</p>
+                    </div>
+                  ))}
+              </>
+            )
+          ) : (
+            <p className="m-4 text-xl font-bold">
+              Must be logged-in to view this page.
+            </p>
+          )}
         </div>
       </div>
     </div>
