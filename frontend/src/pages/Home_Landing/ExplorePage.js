7f076929ead8577213d98866df8899d8df706a12
Huey Nguyen <syndrumablez@gmail.com>
2023-09-06 01:25:28 -0700

    oops, quick styling fix across pages

--- frontend/src/pages/Home_Landing/ExplorePage.js
+++ frontend/src/pages/Home_Landing/ExplorePage.js
@@ -13,7 +13,7 @@ const Explore = () => {
   return (
     <div className="dark:text-light-gray text-secondary-dark-bg bg-light-gray dark:bg-secondary-dark-bg">
       <div className="flex flex-wrap justify-center lg:flex-nowrap">
-        <div className="m-3 flex h-[calc(100dvh-87px)] w-full flex-col items-center justify-center overflow-x-hidden overflow-y-scroll rounded-xl bg-main-bg dark:bg-main-dark-bg md:h-[calc(100dvh-72px)]">
+        <div className="m-3 flex h-[calc(100dvh-72px)] w-full flex-col items-center justify-center overflow-x-hidden overflow-y-scroll rounded-xl bg-main-bg dark:bg-main-dark-bg">
           <p className="m-4 text-xl font-bold">Home for logged-in users</p>
         </div>
       </div>
