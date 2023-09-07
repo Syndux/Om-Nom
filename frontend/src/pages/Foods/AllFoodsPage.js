e12211e56d4ad3c407b7a9c1ccb9e6e7e03acc6b
Huey Nguyen <syndrumablez@gmail.com>
2023-09-06 21:54:44 -0700

    Food form modal open button

--- frontend/src/pages/Foods/AllFoodsPage.js
+++ frontend/src/pages/Foods/AllFoodsPage.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import { OpenModalButton } from "../../components";
 import { loadAllFoods } from "../../store/foods";
+import { FoodFormPage } from "../";
 
 const AllFoodsPage = () => {
   const dispatch = useDispatch();
@@ -23,13 +25,13 @@ const AllFoodsPage = () => {
         <div className="m-3 flex h-[calc(100dvh-72px)] w-full flex-col overflow-x-hidden overflow-y-scroll rounded-xl bg-main-bg dark:bg-main-dark-bg">
           {loaded && (
             <>
-              <div className="flex justify-between m-4 text-xl font-bold items-center">
-                <p>
-                  Browse all available foods
-                </p>
-                <p>
-                  <AiOutlinePlus />
-                </p>
+              <div className="m-4 flex items-center justify-between text-xl font-bold">
+                <p>Browse all available foods</p>
+
+                <OpenModalButton
+                  modalComponent={<FoodFormPage />}
+                  buttonText={<AiOutlinePlus />}
+                />
               </div>
               {foods.map((food) => (
                 <div className="border-t px-10 py-3 duration-100 ease-in hover:scale-[1.01] hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] dark:hover:shadow-[rgba(205,_205,_150,_0.15)_0px_2px_5px_0px,_rgba(255,_255,_255,_0.3)_0px_1px_1px_0px]">
