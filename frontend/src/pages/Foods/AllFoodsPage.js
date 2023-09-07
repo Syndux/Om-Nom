b942837d1b5cb344d49d6c25a36166f65927f539
Huey Nguyen <syndrumablez@gmail.com>
2023-09-06 21:48:18 -0700

    Add food button

--- frontend/src/pages/Foods/AllFoodsPage.js
+++ frontend/src/pages/Foods/AllFoodsPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AiOutlinePlus } from "react-icons/ai";
 
 import { loadAllFoods } from "../../store/foods";
 
@@ -22,9 +23,14 @@ const AllFoodsPage = () => {
         <div className="m-3 flex h-[calc(100dvh-72px)] w-full flex-col overflow-x-hidden overflow-y-scroll rounded-xl bg-main-bg dark:bg-main-dark-bg">
           {loaded && (
             <>
-              <p className="m-4 text-xl font-bold">
-                Browse all available foods
-              </p>
+              <div className="flex justify-between m-4 text-xl font-bold items-center">
+                <p>
+                  Browse all available foods
+                </p>
+                <p>
+                  <AiOutlinePlus />
+                </p>
+              </div>
               {foods.map((food) => (
                 <div className="border-t px-10 py-3 duration-100 ease-in hover:scale-[1.01] hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] dark:hover:shadow-[rgba(205,_205,_150,_0.15)_0px_2px_5px_0px,_rgba(255,_255,_255,_0.3)_0px_1px_1px_0px]">
                   <p className="text-lg">{food.name}</p>
