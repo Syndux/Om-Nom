7cc258e66a7c9c431dfad1a18042825a69b0b2ed
Huey Nguyen <syndrumablez@gmail.com>
2023-09-06 21:49:05 -0700

    Added new type/action/thunk/reducer for creating and editing foods

--- frontend/src/store/foods.js
+++ frontend/src/store/foods.js
@@ -1,6 +1,9 @@
-import { csrfFetch } from './csrf';
+import { csrfFetch } from "./csrf";
 
-const LOAD_ALL_FOODS = 'foods/LOAD_ALL_FOODS';
+const LOAD_ALL_FOODS = "foods/LOAD_ALL_FOODS";
+const LOAD_SINGLE_FOOD = "foods/LOAD_SINGLE_FOOD";
+const CREATE_FOOD = "foods/CREATE_FOOD";
+const UPDATE_FOOD = "foods/UPDATE_FOOD";
 
 // AC - Action Creator
 const loadAllFoodsAC = (foods) => ({
@@ -8,8 +11,24 @@ const loadAllFoodsAC = (foods) => ({
   payload: foods,
 });
 
-// Thunk Action Creator
-export const loadAllFoods = () => async dispatch => {
+const loadSingleFoodAC = (food) => ({
+  type: LOAD_SINGLE_FOOD,
+  payload: food,
+});
+
+const createFoodAC = (food) => ({
+  type: CREATE_FOOD,
+  payload: food,
+});
+
+const updateFoodAC = (food) => ({
+  type: UPDATE_FOOD,
+  payload: food,
+});
+
+// THUNK ACTION CREATOR
+// Get all foods
+export const loadAllFoods = () => async (dispatch) => {
   const res = await csrfFetch("/api/foods");
 
   if (res.ok) {
@@ -17,15 +36,60 @@ export const loadAllFoods = () => async dispatch => {
     dispatch(loadAllFoodsAC(foods));
     return foods;
   }
-}
+};
+
+// Get food details
+export const loadSingleFood = (foodId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/foods/${foodId}`);
+
+  if (res.ok) {
+    const food = await res.json();
+    dispatch(loadSingleFoodAC(food));
+    //return food.id;
+  }
+};
+
+// Create new food
+export const createFood = (foodData) => async (dispatch) => {
+  const res = await csrfFetch("/api/foods", {
+    method: "POST",
+    body: JSON.stringify(foodData),
+  });
+
+  if (res.ok) {
+    const food = await res.json();
+    dispatch(createFoodAC(food));
+    return food.id;
+  }
+};
+
+// Edit a food
+export const updateFood = (foodId, formData) => async (dispatch) => {
+  const res = await csrfFetch(`/api/foods/${foodId}`, {
+    method: "PUT",
+    body: JSON.stringify(formData),
+  });
+
+  if (res.ok) {
+    const food = await res.json();
+    dispatch(updateFoodAC(food));
+    return food.id;
+  }
+};
 
 const initialState = {};
 
 const foodsReducer = (state = initialState, action) => {
-  let newState = { ...state };
   switch (action.type) {
     case LOAD_ALL_FOODS:
+      // return { ...state, ...action.payload };
       return action.payload;
+    case LOAD_SINGLE_FOOD:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_FOOD:
+      return { ...state, [action.payload.id]: action.payload };
+    case UPDATE_FOOD:
+      return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
   }
