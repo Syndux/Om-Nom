80fed5cfac78c363e4d2ee5c4d20677c3f68afbc
Huey Nguyen <syndrumablez@gmail.com>
2023-09-11 00:59:47 -0700

    Edit/delete

--- frontend/src/pages/Ingredients/AllIngredientsPage.js
+++ frontend/src/pages/Ingredients/AllIngredientsPage.js
@@ -28,7 +28,7 @@ const AllIngredientsPage = () => {
   }, [dispatch]);
 
   return (
-    <div className="dark:text-light-gray text-secondary-dark-bg bg-light-gray dark:bg-secondary-dark-bg">
+    <div className="bg-light-gray text-secondary-dark-bg dark:bg-secondary-dark-bg dark:text-light-gray">
       <div className="flex flex-wrap justify-center lg:flex-nowrap">
         <div className="m-3 flex h-[calc(100dvh-71px)] w-full flex-col overflow-x-hidden overflow-y-scroll rounded-xl bg-main-bg dark:bg-main-dark-bg">
           {loaded && (
@@ -44,15 +44,17 @@ const AllIngredientsPage = () => {
                 <OpenModalButton
                   modalComponent={<IngredientFormModal />}
                   buttonText="New Ingredient"
-                  className="bg-blue-700 text-main-bg rounded-lg p-1.5 text-sm font-semibold duration-100 ease-in hover:scale-105"
+                  className="rounded-lg bg-blue-700 p-1.5 text-sm font-semibold text-main-bg duration-100 ease-in hover:scale-105"
                 />
               </div>
               {ingredients.map((ingredient) => (
                 <div
                   key={ingredient.id}
-                  className="border-t px-10 py-3 duration-100 ease-in hover:scale-[1.01] hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] dark:hover:shadow-[rgba(205,_205,_150,_0.15)_0px_2px_5px_0px,_rgba(255,_255,_255,_0.3)_0px_1px_1px_0px]"
+                  className="flex justify-between border-t px-10 py-3 duration-100 ease-in hover:scale-[1.01] hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] dark:hover:shadow-[rgba(205,_205,_150,_0.15)_0px_2px_5px_0px,_rgba(255,_255,_255,_0.3)_0px_1px_1px_0px]"
                 >
-                  <p className="text-lg font-semibold">{ingredient.name}</p>
+                  <div className="max-w-sm overflow-hidden text-ellipsis whitespace-nowrap">
+                    <p className="text-lg font-semibold">{ingredient.name}</p>
+                  </div>
                   {sessionUser && sessionUser?.id === ingredient.creatorId && (
                     <div className="flex flex-row gap-4">
                       <OpenModalButton
@@ -60,13 +62,17 @@ const AllIngredientsPage = () => {
                           <IngredientFormModal ingredientId={ingredient.id} />
                         }
                         buttonText={<AiFillEdit />}
+                        className="text-secondary-dark-bg dark:text-light-gray flex items-center justify-center rounded-lg px-2 text-xl duration-100 ease-in hover:scale-110 hover:bg-light-gray dark:hover:bg-secondary-dark-bg"
                       />
 
                       <OpenModalButton
                         modalComponent={
-                          <ConfirmDeleteIngredientModal ingredientId={ingredient.id} />
+                          <ConfirmDeleteIngredientModal
+                            ingredientId={ingredient.id}
+                          />
                         }
                         buttonText={<FaTrash />}
+                        className="text-secondary-dark-bg dark:text-light-gray flex items-center justify-center rounded-lg px-2 text-base duration-100 ease-in hover:scale-110 hover:bg-light-gray dark:hover:bg-secondary-dark-bg"
                       />
                     </div>
                   )}
