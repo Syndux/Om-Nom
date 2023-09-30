304bff32e24c96e16a602174e4cca019126845d4
Huey Nguyen <syndrumablez@gmail.com>
2023-09-30 13:54:40 -0700

    restyle

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="pt-1 dark:bg-main-dark-bg">
-      <p className="mx-5 text-sm text-gray-700 dark:text-gray-200 pb-1">
-      <Link to="/about">About The Dev</Link>
+      <p className="mx-5 text-sm text-gray-700 dark:text-gray-200 pb-1 flex justify-between">
+        <Link to="/about">About The Dev</Link>
         © 2023 Huey Nguyen. All rights reserved.
       </p>
 
